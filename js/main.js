be73ca52290e6ccde1c2e2801a102b9d6b51411d
Greg <gajdukow@protonmail.com>
2024-04-23 17:43:24 +0200

    Implement timer and Game Instructions

--- js/main.js
+++ js/main.js
@@ -6,39 +6,12 @@ let boardHeight = 636;
 board.style.width = boardWidth + "px";
 board.style.height = boardHeight + "px";
 
-// fly setup
-
-class Fly{
-    constructor(){
-        this.width = 50;
-        this.height = 50;
-        this.minBoardPos = this.width;
-        this.maxBoardPos = boardWidth - this.width;
-        this.XPos = Math.floor(Math.random() * (this.maxBoardPos - this.minBoardPos + 1)) + this.minBoardPos;
-        this.YPos = boardHeight/4;
-        this.DOMElement = null;
-
-        this.createFly()
-    }
-    createFly(){
-        this.DOMElement = document.createElement("div");
-        this.DOMElement.className = "fly"
-        this.DOMElement.style.width = this.width + "px"
-        this.DOMElement.style.height = this.height + "px"
-        this.DOMElement.style.bottom = this.YPos + "px";
-        this.DOMElement.style.left = this.XPos + "px";
-        board.appendChild(this.DOMElement);
-    }
-}
-
-const fly = new Fly();
-
 // class Player
 
 class Player{
     constructor(){
-        this.width = 100;
-        this.height = 100;
+        this.width = 50;
+        this.height = 50;
         this.minBoardPos = this.width;
         this.maxBoardPos = boardWidth - this.width;
         this.XPos = 0
@@ -158,8 +131,10 @@ class Player{
     }
 
     growPlayer(){
+        let oldWidth = this.width
+        let oldHeight = this.height;
         let growPlayerInterval = setInterval(() => {
-        if(this.width <= 120 && this.height <= 120) { // should be flexible (i.e. grow multiple times)
+        if(this.width <= oldWidth+10 && this.height <= oldHeight+10) { // should be flexible (i.e. grow multiple times)
             this.width += 5;
             this.height += 5;
             this.DOMElement.style.width = this.width + "px"
@@ -174,6 +149,44 @@ class Player{
 }
 
 const player = new Player();
+
+// fly setup
+
+class Fly{
+    constructor(ID){
+        this.ID = ID;
+        this.width = 50;
+        this.height = 50;
+        this.minBoardPosX = this.width;
+        this.maxBoardPosX = boardWidth - this.width;
+        this.minBoardPosY = boardHeight/6;
+        this.maxBoardPosY = boardHeight/3;
+        this.XPos = Math.floor(Math.random() * (this.maxBoardPosX - this.minBoardPosX + 1)) + this.minBoardPosX;
+        this.YPos = Math.floor(Math.random() * (this.maxBoardPosY - this.minBoardPosY + 1)) + this.minBoardPosY;
+        this.DOMElement = null;
+
+        this.createFly()
+    }
+    createFly(){
+        this.DOMElement = document.createElement("div");
+        this.DOMElement.className = "fly"
+        this.DOMElement.id = "Fly"+this.ID;
+        this.DOMElement.style.width = this.width + "px"
+        this.DOMElement.style.height = this.height + "px"
+        this.DOMElement.style.bottom = this.YPos + "px";
+        this.DOMElement.style.left = this.XPos + "px";
+        board.appendChild(this.DOMElement);
+    }
+}
+
+const flyArray = [new Fly(0)];
+let ID = 1;
+
+setInterval(() => {
+    let newFly = new Fly(ID);
+    flyArray.push(newFly);
+    ID++;
+}, 2000)
 
 
 // To Do: adjust frog image Direction with direction change
@@ -202,15 +215,42 @@ addEventListener("keydown", (event) => {
 
 function detectCollision(){
 let detectCollisionInterval = setInterval(() => {
+        flyArray.forEach((fly, index) => 
+        {
         if (player.XPos < fly.XPos + fly.width &&
             player.XPos + player.width > fly.XPos &&
             player.YPos < fly.YPos + fly.height &&
             player.YPos + player.height > fly.YPos) {
-                fly.DOMElement.style.display = "none"; // rather remove instance
+                document.querySelector(`#Fly${fly.ID}`).remove();
+                flyArray.splice(index, 1)
                 player.growPlayer();
-                console.log("collision")
                 clearInterval(detectCollisionInterval);
         }
+        if(flyArray.length === 0) location.href = "win.html";
+        })
 }, 100);
 }
 detectCollision();
+
+
+let timeRemaining = 60;
+const timer = document.querySelector("#timer");
+let minutes;
+let seconds;
+
+function updateTime() {
+  // Convert the time remaining in seconds to minutes and seconds, and pad the numbers with zeros if needed
+  minutes = Math.floor(timeRemaining / 60).toString().padStart(2, "0");
+  seconds = (timeRemaining % 60).toString().padStart(2, "0");
+
+  // Display the time remaining in the time remaining container
+  timer.innerText = `Time Remaining: ${minutes}:${seconds}`;
+}
+updateTime();
+
+setInterval(() => {
+    timeRemaining--;
+    updateTime();
+    if(timeRemaining === 0) location.href = "gameover.html";
+}, 1000)
+
